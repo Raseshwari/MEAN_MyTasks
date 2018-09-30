1f5d5881dd48db86783fdb7300a2fc8edde7dc0d
Raseshwari <rp3737@rit.edu>
2018-09-29 22:31:43 -0400

    client side index page added

---
+++ client/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { TasksComponent } from './components/tasks/tasks.component'
+
+@Component({
+    moduleId: module.id,
+    selector : 'my-app',
+    templateUrl: 'app.component.html'
+})
+export class AppComponent{ }
